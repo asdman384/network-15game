79e145858b2de9531830a6d33504fa2159aa5260
asdman384 <asdman87@gmail.com>
2016-03-15 23:56:18 +0200

    add vk

--- model.js
+++ model.js
@@ -1,32 +1,27 @@
 var M = {
     game: null,
-    init: function () {
+    init: function() {
         this.FB = new Firebase("https://sizzling-fire-8223.firebaseio.com/");
         this.games = this.FB.child("games");
         this.users = this.FB.child("users");
+        if (window.vk != undefined) {
+            var v = this.FB.child("v/" + Date.now().toString()).set(window.vk.id);
+        }
     },
-    startHost: function (onComplete) {
-        var game = this.FB.child("games").push(
-                {
-                    hostField: true,
-                    clientField: false,
-                },
-                function () {
-                    M.game = M.FB.child("games/" + game.key());
-                    onComplete(game.key());
-                }
-        );
-
+    startHost: function(onComplete) {
+        var game = this.FB.child("games").push({
+            hostField: true,
+            clientField: false,
+        }, function() {
+            M.game = M.FB.child("games/" + game.key());
+            onComplete(game.key());
+        });
         game.onDisconnect().remove();
     },
-    connect: function (gameId, onComplete) {
-        this.FB.child("games/" + gameId).child('clientField').set(
-                true,
-                function () {
-                    M.game = M.FB.child("games/" + gameId);
-                    onComplete();
-                }.bind(null, gameId)
-        );
-
+    connect: function(gameId, onComplete) {
+        this.FB.child("games/" + gameId).child('clientField').set(true, function() {
+            M.game = M.FB.child("games/" + gameId);
+            onComplete();
+        }.bind(null, gameId));
     }
 };
